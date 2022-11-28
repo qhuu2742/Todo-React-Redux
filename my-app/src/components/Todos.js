709bfc8bdf79baa8aa933d21b832edb35faa7fe1
qhuu2742 <qhuu.official@gmail.com>
2022-11-28 13:39:50 +0700

    adding check data length

--- my-app/src/components/Todos.js
+++ my-app/src/components/Todos.js
@@ -27,19 +27,25 @@ const Todos = () => {
   return (
     <div className="todo-list">
       <TodoForm />
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id} className={todo.completed ? "completed" : ""}>
-            {todo.title}
-            <input
-              type="checkbox"
-              checked={todo.completed}
-              onChange={toggleTodoCompleted.bind(this, todo.id)}
-            />
-            <button onClick={deleteSingleTodo.bind(this, todo.id)}>X</button>
-          </li>
-        ))}
-      </ul>
+      {todos.length ? (
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id} className={todo.completed ? "completed" : ""}>
+              {todo.title}
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={toggleTodoCompleted.bind(this, todo.id)}
+              />
+              <button onClick={deleteSingleTodo.bind(this, todo.id)}>X</button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>
+          <i>No data in here</i>
+        </p>
+      )}
     </div>
   );
 };
